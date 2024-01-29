e4e88fbd9a85ce582e737927c73e571f5e4c77bd
lizhenghao <>
2024-01-29 23:25:50 +0800

    Add some notes.

--- codes/02 C/main.cpp
+++ codes/02 C/main.cpp
@@ -1,8 +1,12 @@
-int plus(int x, int y) {
+int plus1(int x, int y) {
 	return x + y;
 }
 
+int plus2(int x, int y, int z) {
+	return plus1(x, y) + z;
+}
+
 void main() {
-	plus(1, 2);
+	int r = plus2(1, 2, 3);
 	return;
 }
