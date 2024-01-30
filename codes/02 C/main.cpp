a7fec8467e669095d0d6ee07f1d9595045195ffd
lizhenghao <>
2024-01-31 02:31:15 +0800

    Add some notes. 2024/01/31

--- codes/02 C/main.cpp
+++ codes/02 C/main.cpp
@@ -1,12 +1,7 @@
-int plus1(int x, int y) {
-	return x + y;
-}
-
-int plus2(int x, int y, int z) {
-	return plus1(x, y) + z;
-}
+#include<stdio.h>
 
 void main() {
-	int r = plus2(1, 2, 3);
+	char buffer[20] = "Hello, World!";
+	printf("%s\n", buffer);
 	return;
 }
